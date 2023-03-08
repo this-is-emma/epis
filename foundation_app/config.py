a3870d15f80821e6a5a13685c57f9aa4f5767e8a
ThisIsEmma <sakatia.lise@gmail.com>
2023-03-08 15:46:20 -0500

    gg

--- foundation_app/config.py
+++ foundation_app/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config(object):
     """Set environment variables."""
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'foundation_app/static/uploads'
