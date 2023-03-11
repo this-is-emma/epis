fc792201f0fc77f1c3a48b6c9f3ef485412d0eb5
ThisIsEmma <sakatia.lise@gmail.com>
2023-03-10 20:25:45 -0500

    are we there yet?

--- foundation_app/config.py
+++ foundation_app/config.py
@@ -8,6 +8,6 @@ class Config(object):
     """Set environment variables."""
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = 'R356HLpewh0YjX5uXzvY'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'foundation_app/static/uploads'
